79a78f375c0293deaec61ceef58335d91e7c5014
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-22 15:18:33 -0800

    commit tree 0a9a1a7a7af43ada80f8269597e2d92645c0c14b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,8 @@
 class Dog
-
+    def name=(dog_name)
+        @this_dogs_name = dog_name #this is now an instance variable, that is available to all instance methods within the class
+    end
+    def name
+        @this_dogs_name #instance variable
+    end
 end
